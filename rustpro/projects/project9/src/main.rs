4057df0bbd2f723c015aab97a339439808f24c70
Parihar Naresh Singh <pariharsoftwdev@gmail.com>
2026-02-09 00:42:30 +0530

    celan

--- rustpro/projects/project9/src/main.rs
+++ rustpro/projects/project9/src/main.rs
@@ -1,3 +1,64 @@
+/*
+Define a `make_money` function that accepts a mutable
+String reference. The function should concatenate
+the characters "$$$" to the end of the String.
+Invoke the function in `main`.
+ 
+Define a `trim_and_capitalize` function that accepts
+a string slice. It should return a String with
+all whitespace removed and all characters in uppercase.
+Invoke the function in `main`.
+ 
+Define an `elements` function that accepts a string
+slice. It should split the string by all occurrences
+of the `!` symbol and return a vector of the string
+slices. Invoke the function in `main`.
+ 
+Example:
+elements("Gold!Silver!Platinum")
+=> Vector of ["Gold", "Silver", "Platinum"]
+ 
+Define a `get_identity` function. The function should
+ask the user for their first and last name in TWO
+steps (i.e., collect user input twice). Make sure
+to communicate the instructions to the user.
+For each Result enum you receive, call the `expect`
+method and provide a custom error message (like
+"Failed to collect first name"). Return a String
+with the first and last names combined. Invoke
+the `get_identity` function in `main`, and output the
+returned String.
+ 
+Example:
 fn main() {
-    println!("Hello, world!");
+  let name = get_identity();
+   println!("{name}"); // Bill Murray
+}
+*/
+
+
+fn make_money(some_str: &mut String){
+    some_str.push_str("$$$");
+}
+
+fn trim_and_capitalize(s:&str)->String{
+    s.trim().to_uppercase()
+}
+
+fn elements(s:&str)->Vec<&str>{
+    s.split('!').collect()
+}
+
+fn main() {
+    println!("Hello, understanding the string stuff!");
+    let mut some_str = String::from("345");
+    make_money(&mut some_str);
+
+    println!("{}", some_str);
+
+    let som="     something absoluter crap a.     ";
+    println!("{} and {}",trim_and_capitalize(som), som);
+
+    println!("{:?}",elements("Gold!Silver!Platinum"))
+
 }
